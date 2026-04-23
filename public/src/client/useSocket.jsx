66528c8fbe6066610079420188f6f56945d22d83
benjamin137 <jvalenzuela1@ucol.mx>
2026-04-22 19:04:39 -0600

    Changed logic socket

--- public/src/client/useSocket.jsx
+++ public/src/client/useSocket.jsx
@@ -1,51 +1,33 @@
-// useSocket.js
-import { io } from 'socket.io-client';
-import { useState, useEffect, useRef } from 'react';
-
-const URL = 'http://localhost:3000';
+import { useEffect, useState } from 'react';
+import { socket } from './socketInstance';
 
 export const useSocket = () => {
 
     const [chatMessages, setChatMessages] = useState([]);
-    const socketRef = useRef(null);
 
     useEffect(() => {
-        socketRef.current = io(URL, {
-            autoConnect: false
-        });
-
-        const socket = socketRef.current;
-
-        socket.on('connect', () => {
-            console.log('✅ Conectado:', socket.id);
-        });
-
-        socket.on('mi_evento', (data) => {
-            console.log('🔥 Evento:', data);
-        });
 
         socket.on('chatMessage', (msg) => {
             if (msg.user && msg.message) {
                 setChatMessages(prev => [...prev, msg]);
             }
-        }); 
+        });
 
         return () => {
-            socket.disconnect();
+            socket.off('chatMessage');
         };
 
     }, []);
 
-    const conectar = () => socketRef.current.connect();
-
-    const enviarMensaje = (msg) => {
-        console.log('Enviando mensaje:', msg);
-        socketRef.current.emit('chatMessage', msg);
+    const conectar = () => {
+        if (!socket.connected) {
+            socket.connect();
+        }
     };
 
     return {
         conectar,
-        enviarMensaje,
+        socket,
         chatMessages
     };
 };
